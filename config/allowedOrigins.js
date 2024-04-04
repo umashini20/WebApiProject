ef76b44af526ada6dea5c1618917a41af16a44fc
umashini20 <umashini2000@gmail.com>
2024-04-04 21:43:22 +0530

    update on origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
+    'http://localhost:3000/weather',
     'https://spotless-hen-sari.cyclic.app',
     'http://localhost:3000/login'
 ];
