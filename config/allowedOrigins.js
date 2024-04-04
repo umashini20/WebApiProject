52d6f3a2fb95fef3a67f19b687f146e8dabf0fce
umashini20 <umashini2000@gmail.com>
2024-04-04 20:37:09 +0530

    Update on corse

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
+    'https://spotless-hen-sari.cyclic.app',
     'http://localhost:3000/login'
 ];
 
